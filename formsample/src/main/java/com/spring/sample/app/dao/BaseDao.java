bfb42d3a7c81d785bd08d7b35cc382602675f6ed
Sandeep <mailme.sandeeps@gmail.com>
2016-11-24 19:05:40 +0530

    Add files via upload

---
+++ formsample/src/main/java/com/spring/sample/app/dao/BaseDao.java
@@ -0,0 +1,5 @@
+package com.spring.sample.app.dao;
+
+public class BaseDao {
+
+}
---
+++ formsample/src/main/java/com/spring/sample/app/service/BaseService.java
@@ -0,0 +1,8 @@
+package com.spring.sample.app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BaseService {
+
+}
